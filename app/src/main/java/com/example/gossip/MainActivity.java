a5ae4dcb2964f965ef8449ddc7d54a13850de078
Sanjana Dubey <spdubey6178@gmail.com>
2022-02-15 15:37:29 +0530

    Navbar completed

--- app/src/main/java/com/example/gossip/MainActivity.java
+++ app/src/main/java/com/example/gossip/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.gossip;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemReselectedListener(navListener);
     }
+    private BottomNavigationView.OnNavigationItemReselectedListener navListener =
+            new BottomNavigationView.OnNavigationItemReselectedListener() {
+                @Override
+                public void onNavigationItemReselected(@NonNull MenuItem item) {
+                    Fragment selectedFrag = null;
+                    switch (item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFrag = new home_page();
+                            break;
+                        case R.id.nav_chat:
+                            selectedFrag = new chat_page();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFrag = new profile_page();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, selectedFrag).commit();
+                }
+            };
 }
