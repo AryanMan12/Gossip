0e6b905e715b7b790ecfb9998a24b57dd5ef8981
Jyothi <jyothimudalu@gmail.com>
2022-02-16 00:06:56 +0530

    Commit activity_signup_page.xml Commit signup_page.java

--- app/src/main/java/com/example/gossip/signup_page.java
+++ app/src/main/java/com/example/gossip/signup_page.java
@@ -3,6 +3,7 @@ package com.example.gossip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class signup_page extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class signup_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
+    }
+
+    public void verifyotp(View view) {
+    }
+
+    public void signup(View view) {
     }
 }
