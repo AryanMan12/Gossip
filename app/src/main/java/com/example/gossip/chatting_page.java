5df3b512ae224be432b014bb9232d107a729294f
Sanchit <sanchitgurav100@gmail.com>
2022-02-15 22:14:16 +0530

    adding chatting page

---
+++ app/src/main/java/com/example/gossip/chatting_page.java
@@ -0,0 +1,14 @@
+package com.example.gossip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chatting_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatting_page);
+    }
+}
