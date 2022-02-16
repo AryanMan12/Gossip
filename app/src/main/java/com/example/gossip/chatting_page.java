a8d62d1e818d5fd18a081c5cdef597f7a21346a4
Sanchit <sanchitgurav100@gmail.com>
2022-02-16 20:41:59 +0530

    changes

--- app/src/main/java/com/example/gossip/chatting_page.java
+++ app/src/main/java/com/example/gossip/chatting_page.java
@@ -2,16 +2,35 @@ package com.example.gossip;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.gossip.databinding.ActivityChattingPageBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class chatting_page extends AppCompatActivity {
     ActivityChattingPageBinding binding;
+    FirebaseDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityChattingPageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+        String senderId = auth.getUid();
+        String receiveId = getIntent().getStringExtra("userId");
+        String userName = getIntent().getStringExtra("userName");
+        String profilePic = getIntent().getStringExtra("profilePic");
+        binding.userName.setText(userName);
+        Picasso.get().load(profilePic).placeholder(R.drawable.ic_action_name).into(binding.profileImage);
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(chatting_page.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
