137ef3182d0a35bc783fb8ac694001037d500046
Sanjana Dubey <spdubey6178@gmail.com>
2022-02-17 17:33:01 +0530

    Login page

--- app/src/main/java/com/example/gossip/Login.java
+++ app/src/main/java/com/example/gossip/Login.java
@@ -3,6 +3,7 @@ package com.example.gossip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onLogin(View view) {
     }
 }
