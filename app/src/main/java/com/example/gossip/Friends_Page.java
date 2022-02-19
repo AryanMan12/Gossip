8cf20ad19addb80f2b4e2539786a0e6690a9cd74
Sanjana Dubey <spdubey6178@gmail.com>
2022-02-19 19:50:09 +0530

    Home page fragments done

---
+++ app/src/main/java/com/example/gossip/Friends_Page.java
@@ -0,0 +1,20 @@
+package com.example.gossip;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Friends_Page extends Fragment {
+    View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_friends__page, container, false);
+        return view;
+    }
+}
