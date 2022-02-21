6c7c836624b14d881ad19c74fa4e7a043a483ef8
Sanjana Dubey <spdubey6178@gmail.com>
2022-02-22 00:52:56 +0530

    Dialog box friends page

--- app/src/main/java/com/example/gossip/adaptor/RecyclerViewAdaptor.java
+++ app/src/main/java/com/example/gossip/adaptor/RecyclerViewAdaptor.java
@@ -3,18 +3,25 @@ package com.example.gossip.adaptor;
 import static com.example.gossip.R.menu.friends_more_btn;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -87,10 +94,29 @@ public class RecyclerViewAdaptor extends RecyclerView.Adapter<RecyclerViewAdapto
         holder.profileimg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(context, ViewProfile.class);
-                intent.putExtra("username",user.getUsername());
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(intent);
+
+                final Dialog dialog=new Dialog(context);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.view_profile_dialog);
+
+                ImageView gallery_img =dialog.findViewById(R.id.profile_dialog);
+                TextView view_full_profile=dialog.findViewById(R.id.view_profile_dialog);
+
+                view_full_profile.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(context, ViewProfile.class);
+                        intent.putExtra("username",user.getUsername());
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        context.startActivity(intent);
+                    }
+                });
+
+                dialog.show();
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.getWindow().getAttributes().windowAnimations= R.style.DialogAnimation;
+                dialog.getWindow().setGravity(Gravity.CENTER);
             }
         });
 
@@ -114,6 +140,9 @@ public class RecyclerViewAdaptor extends RecyclerView.Adapter<RecyclerViewAdapto
                                 intent1.putExtra("username",user.getUsername());
                                 intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                                 context.startActivity(intent1);
+                                break;
+                            case R.id.remove_fr:
+                                Toast.makeText(context, "Removed", Toast.LENGTH_SHORT).show();
                                 break;
                         }
                         return false;
