7ee98c2200182af86991ab1f9095e79159e80bc2
AryanMan12 <aryanaryan1212.ag@gmail.com>
2022-03-05 18:54:24 +0530

    Notification Feature added

--- app/src/main/java/com/example/gossip/notification/FireBaseMessagingService.java
+++ app/src/main/java/com/example/gossip/notification/FireBaseMessagingService.java
@@ -1,10 +1,14 @@
 package com.example.gossip.notification;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -15,16 +19,23 @@ public class FireBaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        String CHANNEL_ID="MESSAGE";
+        String CHANNEL_NAME="MESSAGE";
+        NotificationManagerCompat manager=NotificationManagerCompat.from(getApplicationContext());
         title=remoteMessage.getData().get("Title");
         message=remoteMessage.getData().get("Message");
 
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(getApplicationContext())
-                        .setSmallIcon(R.drawable.application_icon)
-                        .setContentTitle(title)
-                        .setContentText(message);
-        NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(0, builder.build());
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel=new NotificationChannel(CHANNEL_ID,CHANNEL_NAME,NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(channel);
+        }
+        Notification notification = new NotificationCompat.Builder(getApplicationContext(),CHANNEL_ID)
+                .setSmallIcon(R.drawable.application_icon)
+                .setContentTitle(title)
+                .setContentText(message)
+                .build();
+
+        manager.notify(0, notification);
     }
 
 }
